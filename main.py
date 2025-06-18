b6ac4d1526c113dc2b8532a09969544d9a0b7295
rokia876 <rokiahossam11@gmail.com>
2025-06-18 21:41:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+guest={"Rokia","Nesma","Mahmoud","Takwa"}
+name=input("please inter your name:")
+if name in guest:
+    print("Hello welcome to your class ☺️")
+if name not in guest:
+    print("Sorry, couldn't find your name in the class list 😕")
+    
